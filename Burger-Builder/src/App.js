c83b29661d58dfebb32141b7bcec40cab5e6e5fb
Jobayer Al Mahmud Tuser <jtuser112@gmail.com>
2020-11-24 11:36:49 +0600

    new update

--- Burger-Builder/src/App.js
+++ Burger-Builder/src/App.js
@@ -1,11 +1,21 @@
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div>
-      
-    </div>
-  );
+import './App.css';
+import Layout from './components/layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+
+class App extends Component{
+
+  render(){
+    return (
+      <div>
+        <Layout>
+          <BurgerBuilder></BurgerBuilder>
+        </Layout>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
